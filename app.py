9b47f6b520f4242ba809ca15d5c0cc4c697e3c3a
Nikhil Singh <51564808+Nick-1998@users.noreply.github.com>
2023-01-11 01:24:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,14 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open(X_test,'model.pkl', 'rb'))
+
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Thyroid has paitenit or nit be $ {}'.format(output))
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
